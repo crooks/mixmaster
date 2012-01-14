0ba0a605728134e34c085f009f90077684e489c3
Steve Crook <steve@mixmin.net>
2012-01-14 11:52:03 +0000

    We want esub in capstrings, regardless of IDEA support.

--- Mix/Src/stats.c
+++ Mix/Src/stats.c
@@ -416,9 +416,7 @@ void conf_premail(BUFFER *out)
 #endif /* USE_IDEA */
     buf_appends(out, " ekx");
   }
-#ifdef USE_IDEA
   buf_appends(out, " esub");
-#endif /* USE_IDEA */
   buf_appends(out, " hsub");
 #if 0				/* obsolete */
 #ifdef USE_NSUB
